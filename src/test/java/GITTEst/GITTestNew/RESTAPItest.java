87a18332efe4f7b38a019d63f4dff3633d30da48
Jalpa02 <jalpaboghani22@gmail.com>
2022-11-08 14:42:48 +0530

    comment08

---
+++ src/test/java/GITTEst/GITTestNew/RESTAPItest.java
@@ -0,0 +1,20 @@
+package GITTEst.GITTestNew;
+
+public class RESTAPItest {
+
+	public void postjira()
+	{
+	
+		System.out.println("postjira");
+		
+		System.out.println("postjira1");
+		System.out.println("postjira2");
+	}
+
+	
+	public void deleteTwitter()
+	{
+		System.out.println("Delete Twitter");
+	}
+	
+}
